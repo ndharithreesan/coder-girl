ab2dbf65291e13a71e341df9687b8d9152473925
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-06-04 13:02:29 -0700

    (fix) Fix typo in object

--- documentation/challenges/whileLoop.js
+++ documentation/challenges/whileLoop.js
@@ -2,9 +2,9 @@
 
 
 {
-  title: 'While loop party'
+  "title": 'While loop party',
 
-  instructions: "It's Friday night and you're at a party thrown by your crush! Your romantic rival Bianca is at the party too (oh no!)! You decide to go toe to toe 
+  "instructions": "It's Friday night and you're at a party thrown by your crush! Your romantic rival Bianca is at the party too (oh no!)! You decide to go toe to toe 
   with her the whole night. Soon, you're the life of the party! You're dancing the night away, making new friends, and catching the eye of a certain someone. 
   Except there's one problem- you overhear Bianca saying that she's going to wait until the end of the night to swoop in and steal your crush! You decide to wait 
   even longer than her! \n \nA while loop is a loop that takes in a conditional and will only run if that condition is met. \n \n Write a function that takes in the number 
@@ -12,12 +12,12 @@
   are people, you should let people know you're not leaving the party by console logging it. After everyone is gone, you should shout that you win (take that, Bianca!) 
   by returning 'I win!'.",
 
-  content: "var partyMonster = function(guests){ \n //we have provided the skeleton of a while loop, to help \n while(SOME_CONDITION){\n //do some work\n //make 
+  "content": "var partyMonster = function(guests){ \n //we have provided the skeleton of a while loop, to help \n while(SOME_CONDITION){\n //do some work\n //make 
       sure to change your condition in some way (eg: decrement the count if you're waiting until it hits 0)\n}\n}",
 
-  testCode: 3,
+ " testCode": 3,
 
-  pointvalue: 100
+ " pointvalue": 100
 }
 
 
